c642a7dd18d405f097a07bf4cfb25524ce1e5b49
tw31120071857 <93863760+tw31120071857@users.noreply.github.com>
2022-10-07 12:17:11 +1100

    Changed core hour/minute mechanics.

--- main.ts
+++ main.ts
@@ -1,148 +1,62 @@
 input.onButtonPressed(Button.A, function () {
     if (page == 1) {
-        basic.showString("" + (time))
+        basic.showString("" + (time_minute))
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+        basic.showString("" + (time_hour))
     }
     if (page == 99) {
-        selecting_hour += 1
-        basic.showString("" + (selecting_hour))
+        changing_hour += 1
+        basic.showString("" + (changing_hour))
     }
 })
 input.onButtonPressed(Button.AB, function () {
     if (page == 1) {
-        basic.showString("" + (selecting_hour))
+        basic.showString("" + (changing_hour))
         page = 99
+        changing_time = 1
     }
 })
 input.onButtonPressed(Button.B, function () {
-    finished_selecting_hour_2 = 1
     if (page == 99) {
-        if (time > 1159) {
-            time = time - 1200
-        }
-        if (time > 1059) {
-            time = time - 1100
-        }
-        if (time > 959) {
-            time = time - 1000
-        }
-        if (time > 859) {
-            time = time - 900
-        }
-        if (time > 759) {
-            time = time - 800
-        }
-        if (time > 659) {
-            time = time - 700
-        }
-        if (time > 559) {
-            time = time - 600
-        }
-        if (time > 459) {
-            time = time - 500
-        }
-        if (time > 359) {
-            time = time - 400
-        }
-        if (time > 259) {
-            time = time - 300
-        }
-        if (time > 159) {
-            time = time - 200
-        }
-        if (time > 59) {
-            time = time - 100
+        if (changing_time == 1) {
+            time_hour = changing_hour
         }
     }
 })
-let finished_selecting_hour = 0
-let finished_selecting_hour_2 = 0
-let selecting_hour = 0
-let time = 0
+let changing_time = 0
+let changing_hour = 0
+let time_minute = 0
+let time_hour = 0
 let page = 0
 page = 1
-time = 1160
-selecting_hour = 1
-finished_selecting_hour_2 = 0
+time_hour = 12
+time_minute = 30
+changing_hour = 1
+// 0 =  Off
+// 1 =  Setting the hour.
+// 2 = Setting the minute.
+changing_time = 0
 basic.forever(function () {
-    if (time == 1160) {
-        time = 1200
+    if (time_minute == 60) {
+        time_minute = 0
+        time_hour += 1
     }
-    if (time == 1260) {
-        time = 100
-    }
-    if (time == 160) {
-        time = 200
-    }
-    if (time == 260) {
-        time = 300
-    }
-    if (time == 360) {
-        time = 400
-    }
-    if (time == 460) {
-        time = 500
-    }
-    if (time == 560) {
-        time = 600
-    }
-    if (time == 660) {
-        time = 700
-    }
-    if (time == 760) {
-        time = 800
-    }
-    if (time == 860) {
-        time = 900
-    }
-    if (time == 960) {
-        time = 1000
-    }
-    if (time == 1060) {
-        time = 1100
+    if (time_hour == 13) {
+        time_hour = 1
     }
 })
 basic.forever(function () {
-    if (selecting_hour == 13) {
-        selecting_hour = 1
-    }
+    basic.pause(60000)
+    time_minute += 1
 })
 basic.forever(function () {
-    if (finished_selecting_hour_2 == 1) {
-        if (selecting_hour == 1) {
-            finished_selecting_hour = 100
-        }
-        if (selecting_hour == 2) {
-            finished_selecting_hour = 200
-        }
-        if (selecting_hour == 3) {
-            finished_selecting_hour = 300
-        }
-        if (selecting_hour == 4) {
-            finished_selecting_hour = 400
-        }
-        if (selecting_hour == 5) {
-            finished_selecting_hour = 500
-        }
-        if (selecting_hour == 6) {
-            finished_selecting_hour = 600
-        }
-        if (selecting_hour == 7) {
-            finished_selecting_hour = 700
-        }
-        if (selecting_hour == 8) {
-            finished_selecting_hour = 800
-        }
-        if (selecting_hour == 9) {
-            finished_selecting_hour = 900
-        }
-        if (selecting_hour == 10) {
-            finished_selecting_hour = 1000
-        }
-        if (selecting_hour == 11) {
-            finished_selecting_hour = 1100
-        }
-        if (selecting_hour == 12) {
-            finished_selecting_hour = 1200
-        }
+    if (changing_hour == 13) {
+        changing_hour = 1
     }
 })
